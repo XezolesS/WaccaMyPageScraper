38c63a59bca8a4e5bd02d627fbba1751b3e86881
Joe K <tndid7876@gmail.com>
2022-08-20 23:11:26 +0900

    Console command features to test efficiently.

--- WaccaMyPageScraper.Console/Program.cs
+++ WaccaMyPageScraper.Console/Program.cs
@@ -14,38 +14,54 @@ Log.Logger = new LoggerConfiguration()
 PageConnector pageConnector;
 
 // Retreive Aime Id
+int loginTries = 0;
 while (true)
 {
+    if (loginTries >= 10)
+    {
+        Log.Error("Can not login to WACCA my page. Check your internet connection");
+
+        return;
+    }
+
     var aimeId = GetAimeId();
     var isLoginSuccess = await LoginToPageAsync(aimeId);
 
     if (isLoginSuccess)
     {
         Log.Information("Login succeed!");
+
         break;
     }
 
     Log.Error("Login failed!");
+    loginTries++;
 }
 
-var playerFetcher = new PlayerFetcher(pageConnector);
-var player = await playerFetcher.FetchAsync();
-Log.Information("${PlayerData}", player);
-
-var musicFetcher = new MusicsFetcher(pageConnector);
-var musics = await musicFetcher.FetchAsync();
-
-Log.Information("{Musics} of Musics have been fetched", musics.Length);
-
-int count = 0;
-foreach (var music in musics)
+while (true)
 {
-    var musicDetailFetcher = new MusicDetailFetcher(pageConnector);
-    var musicDetail = await musicDetailFetcher.FetchAsync(music.Id, music.Genre);
+    Console.WriteLine("Type a number of a task which you want to do.\n");
+    Console.WriteLine("\t(1) Fetch player's data");
+    Console.WriteLine("\t(2) Fetch player's music records");
+    Console.WriteLine("\t(0) Exit");
+    Console.WriteLine("");
 
-    Log.Information("{Count} out of {Musics} has been fetched: \n{MusicDetail}", ++count, musics.Length, musicDetail);
+    Console.Write(" >> ");
+    string input = Console.ReadLine();
+    if (!int.TryParse(input, out int result))
+    {
+        Log.Warning("Input must be a integer value.");
 
-    Thread.Sleep(100);
+        continue;
+    }
+
+    switch (int.Parse(input))
+    {
+        case 0: return;
+        case 1: Log.Information("${PlayerData}", await FetchPlayerAsync()); break;
+        case 2: await LogMusicDetailsAsync(); break;
+        default: break;
+    }
 }
 
 Log.CloseAndFlush();
@@ -75,5 +91,33 @@ async Task<bool> LoginToPageAsync(string aimeId)
     result = await pageConnector.LoginAsync();
     
     return result;
+}
+
+async Task<Player> FetchPlayerAsync()
+{
+    var playerFetcher = new PlayerFetcher(pageConnector);
+
+    return await playerFetcher.FetchAsync(); ;
+}
+
+async Task<Music[]> LogMusicDetailsAsync()
+{
+    var musicFetcher = new MusicsFetcher(pageConnector);
+    var musics = await musicFetcher.FetchAsync();
+
+    Log.Information("{Musics} of Musics have been fetched", musics.Length);
+
+    int count = 0;
+    foreach (var music in musics)
+    {
+        var musicDetailFetcher = new MusicDetailFetcher(pageConnector);
+        var musicDetail = await musicDetailFetcher.FetchAsync(music.Id, music.Genre);
+
+        Log.Information("{Count} out of {Musics} has been fetched: \n{MusicDetail}", ++count, musics.Length, musicDetail);
+
+        Thread.Sleep(100);
+    }
+
+    return musics;
 }
 #endregion
